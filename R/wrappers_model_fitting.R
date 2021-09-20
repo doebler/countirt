e0bfc93f03573bab7232a7045844b6860c541b48
marie <marie.beisemann@web.de>
2021-09-20 15:32:55 +0200

    fix poisson start values

--- R/wrappers_model_fitting.R
+++ R/wrappers_model_fitting.R
@@ -30,10 +30,12 @@ cirt <- function(data, family, n_nodes = 121, stand_errors = FALSE,
   # TODO implement some proper error catching and meaningful error messages
   
   if (family == "cmp") {
+    Print("Start determining start values.")
     start_values <- get_start_values(
       data = data, init_disp_one = control$init_disp_one
     )
     
+    Print("Start model fitting. This will at least take a little bit of time.")
     fit <- run_newem(
       data = data, 
       init_params = start_values, 
@@ -60,9 +62,10 @@ cirt <- function(data, family, n_nodes = 121, stand_errors = FALSE,
     }
   } else if (family == "poisson") {
     start_values <- get_start_values_pois(
-      data = data, init_disp_one = control$init_disp_one
+      data = data
     )
     
+    Print("Start model fitting.")
     fit <- run_em_poisson(
       data = data, 
       init_params = start_values, 
